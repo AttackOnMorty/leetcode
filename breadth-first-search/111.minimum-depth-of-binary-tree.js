075ac954f386ee87f2131a02d76271f0b015e5fa
pfmao <ErenJagerM@outlook.com>
2021-01-11 14:55:27 +0800

    [111] Minimum Depth of Binary Tree

---
+++ breadth-first-search/111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=111 lang=javascript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// BFS
+function minDepth(root) {
+  if (root === null) return 0
+  const queue = []
+  queue.push(root)
+  let depth = 1
+  while (queue.length !== 0) {
+    const length = queue.length
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift()
+      if (node.left === null && node.right === null) return depth
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    depth++
+  }
+}
+
+// @lc code=end
