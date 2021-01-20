0da2ea87064ba2c058f2576e294a95dad7d8f5e0
pfmao <ErenJagerM@outlook.com>
2021-01-20 14:45:29 +0800

    [188] Best Time to Buy and Sell Stock IV

---
+++ dynamic-programming/stock/188.best-time-to-buy-and-sell-stock-iv.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=188 lang=javascript
+ *
+ * [188] Best Time to Buy and Sell Stock IV
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+function maxProfit(max_k, prices) {
+  if (max_k === 0 || prices.length === 0) return 0
+  const n = prices.length
+  const dp = []
+  for (let i = 0; i < n; i++) {
+    let temp = 0
+    for (let k = 1; k <= max_k; k++) {
+      if (i === 0) {
+        addToDP(i, k, 0, 0)
+        addToDP(i, k, 1, -prices[i])
+        console.log(`i: ${i}, k: ${k}`, dp[i][k][0], dp[i][k][1])
+        continue
+      }
+      addToDP(i, k, 0, Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]))
+      addToDP(i, k, 1, Math.max(dp[i - 1][k][1], temp - prices[i]))
+      temp = dp[i - 1][k][0]
+      console.log(`i: ${i}, k: ${k}`, dp[i][k][0], dp[i][k][1])
+    }
+  }
+  return dp[n - 1][max_k][0]
+
+  function addToDP(i, j, k, item) {
+    if (dp[i] === undefined) dp[i] = []
+    if (dp[i][j] === undefined) dp[i][j] = []
+    dp[i][j][k] = item
+  }
+}
+
+// @lc code=end
