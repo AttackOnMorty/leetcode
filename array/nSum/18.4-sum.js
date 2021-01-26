881249dbadfc308769881a02e909ed3dae710423
pfmao <ErenJagerM@outlook.com>
2021-01-26 17:20:18 +0800

    add nSum solution

--- array/nSum/18.4-sum.js
+++ array/nSum/18.4-sum.js
@@ -63,4 +63,46 @@ function fourSum(nums, target) {
   return res
 }
 
+// nSum
+function nSum(n, nums, start, target) {
+  nums.sort((a, b) => a - b)
+
+  const res = []
+  const numbers = nums.slice(start)
+  const size = numbers.length
+  if (n === 2) {
+    let i = 0
+    let j = size - 1
+    while (i < j) {
+      const left = numbers[i]
+      const right = numbers[j]
+      const sum = left + right
+      if (sum === target) {
+        res.push([left, right])
+        while (i < j && numbers[i] === left) i++
+        while (i < j && numbers[j] === right) j--
+      } else if (sum > target) {
+        while (i < j && numbers[j] === right) j--
+      } else if (sum < target) {
+        while (i < j && numbers[i] === left) i++
+      }
+    }
+    return res
+  } else {
+    for (let i = 0; i < size; i++) {
+      const curr = numbers[i]
+      const arrays = nSum(n - 1, numbers, i + 1, target - curr)
+      for (const array of arrays) {
+        res.push([curr, ...array])
+      }
+      while (i < size - 1 && numbers[i + 1] === curr) i++
+    }
+    return res
+  }
+}
+
+function fourSum(nums, target) {
+  return nSum(4, nums, 0, target)
+}
+
 // @lc code=end
