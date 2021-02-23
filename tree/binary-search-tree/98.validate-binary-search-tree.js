ec0500aba48ade6df53a158b1acb9092bddb747a
pfmao <ErenJagerM@outlook.com>
2021-02-23 19:23:12 +0800

    [98] Validate Binary Search Tree

---
+++ tree/binary-search-tree/98.validate-binary-search-tree.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=98 lang=javascript
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+function isValidBST(root) {
+    return isValid(root, null, null);
+}
+
+function isValid(root, min, max) {
+    if (root === null) return true;
+    if (min && root.val <= min.val) return false;
+    if (max && root.val >= max.val) return false;
+    return isValid(root.left, min, root) && isValid(root.right, root, max);
+}
+
+// @lc code=end
