724e6bfb1ef5488139d642bc964c95e9c4df9998
pfmao <ErenJagerM@outlook.com>
2021-02-23 19:29:01 +0800

    [700] Search in a Binary Search Tree

---
+++ tree/binary-search-tree/700.search-in-a-binary-search-tree.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=700 lang=javascript
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+function searchBST(root, val) {
+    if (root === null) return root;
+    if (val === root.val) {
+        return root;
+    } else if (val < root.val) {
+        return searchBST(root.left, val);
+    } else if (val > root.val) {
+        return searchBST(root.right, val);
+    }
+}
+
+// @lc code=end
