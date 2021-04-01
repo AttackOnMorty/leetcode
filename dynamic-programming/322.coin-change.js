10b936b2de1f23150404ca089c35c057b9c9cfbc
pfmao <ErenJagerM@outlook.com>
2021-04-01 16:31:57 +0800

    refactor 509 and 322

--- dynamic-programming/322.coin-change.js
+++ dynamic-programming/322.coin-change.js
@@ -11,55 +11,17 @@
  * @return {number}
  */
 
-// Recursion v1
+// DP  O(n) / O(n)
 function coinChange(coins, amount) {
-  return dp(amount)
-
-  function dp(amount) {
-    if (amount < 0) return -1
-    if (amount === 0) return 0
-    let res = Infinity
-    for (const coin of coins) {
-      const subProblem = dp(amount - coin)
-      if (subProblem === -1) continue
-      res = Math.min(res, 1 + subProblem)
+    const dp = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+    for (let i = 0; i <= amount; i++) {
+        for (const coin of coins) {
+            if (i - coin < 0) continue;
+            dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+        }
     }
-    return res === Infinity ? -1 : res
-  }
-}
-
-// Recursion v1: cache
-function coinChange(coins, amount) {
-  const cache = new Map()
-  return dp(amount, cache)
-
-  function dp(amount) {
-    if (amount < 0) return -1
-    if (amount === 0) return 0
-    if (cache.has(amount)) return cache.get(amount)
-    let res = Infinity
-    for (const coin of coins) {
-      const subProblem = dp(amount - coin)
-      if (subProblem === -1) continue
-      res = Math.min(res, 1 + subProblem)
-    }
-    cache.set(amount, res === Infinity ? -1 : res)
-    return cache.get(amount)
-  }
-}
-
-// DP
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(Infinity)
-  dp[0] = 0
-  for (let i = 0; i <= amount; i++) {
-    for (const coin of coins) {
-      if (i - coin < 0) continue
-      const temp = 1 + dp[i - coin]
-      dp[i] = Math.min(dp[i], temp)
-    }
-  }
-  return dp[amount] === Infinity ? -1 : dp[amount]
+    return dp[amount] === Infinity ? -1 : dp[amount];
 }
 
 // @lc code=end
--- dynamic-programming/509.fibonacci-number.js
+++ dynamic-programming/509.fibonacci-number.js
@@ -10,48 +10,39 @@
  * @return {number}
  */
 
-// Recursion v1
+// Recursion  O(2^n) / O(1)
 function fib(n) {
-  if (n === 0 || n === 1) return n
-  return fib(n - 1) + fib(n - 2)
+    if (n === 0 || n === 1) return n;
+    return fib(n - 1) + fib(n - 2);
 }
 
-// Recursion v2: cache
+// Recursion with cache  O(n) / O(n)
 function fib(n) {
-  const cache = new Map()
-  return helper(n)
+    const cache = new Map();
+    return helper(n);
 
-  function helper(n) {
-    if (n === 0 || n === 1) return n
-    if (cache.has(n)) return cache.get(n)
-    cache.set(n, helper(n - 1, cache) + helper(n - 2, cache))
-    return cache.get(n)
-  }
+    function helper(n) {
+        if (n === 0 || n === 1) return n;
+        if (cache.has(n)) return cache.get(n);
+
+        const res = fib(n - 1) + fib(n - 2);
+        cache.set(n, res);
+
+        return res;
+    }
 }
 
-// DP v1
+// DP  O(n) / O(1)
 function fib(n) {
-  if (n === 0 || n === 1) return n
-  const dp = []
-  dp[0] = 0
-  dp[1] = 1
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2]
-  }
-  return dp[n]
-}
-
-// DP v2: compressed
-function fib(n) {
-  if (n === 0 || n === 1) return n
-  let prev = 0
-  let curr = 1
-  for (let i = 2; i <= n; i++) {
-    const sum = prev + curr
-    prev = curr
-    curr = sum
-  }
-  return curr
+    if (n === 0 || n === 1) return n;
+    let prev = 0;
+    let curr = 1;
+    for (let i = 2; i <= n; i++) {
+        const temp = prev + curr;
+        prev = curr;
+        curr = temp;
+    }
+    return curr;
 }
 
 // @lc code=end
