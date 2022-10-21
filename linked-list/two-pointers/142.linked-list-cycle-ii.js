f86cb6b8e4d1ef872eab00201089022218e32074
Luke Mao [SSW] <ErenJagerM@outlook.com>
2022-10-21 16:06:49 +0800

    Finish 876, 141, 142

---
+++ linked-list/two-pointers/142.linked-list-cycle-ii.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=142 lang=javascript
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// O(n) / O(1)
+function detectCycle(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            break;
+        }
+    }
+
+    if (fast === null || fast.next === null) {
+        return null;
+    }
+
+    slow = head;
+
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+}
+
+// @lc code=end
