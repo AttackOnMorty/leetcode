63198e46ab34a928d9c15ecab2ab1463cc97e094
Luke Mao [SSW] <ErenJagerM@outlook.com>
2022-10-22 09:25:36 +0800

    Finish 26

---
+++ array/two-pointers/26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// O(n) / O(1)
+function removeDuplicates(nums) {
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    return k;
+}
+
+// @lc code=end
