80a585625eacb88d8853285acfa14accf879335e
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-05-29 09:06:11 +0800

    Redo 931

--- advanced/dynamic-programming/931.minimum-falling-path-sum.js
+++ advanced/dynamic-programming/931.minimum-falling-path-sum.js
@@ -10,30 +10,28 @@
  * @return {number}
  */
 
-/**
- * dp[i][j] = dp[i][j] + min(dp[i-1][j-1], dp[i-1][j], dp[i-1][j+1])
- */
+// dp[row][col] += min(dp[row-1][col-1], dp[row-1][col], dp[row-1][col+1])
+
+// O(n^2) / O(1)
 function minFallingPathSum(matrix) {
-    const n = matrix.length;
+  const n = matrix.length;
 
-    matrix.forEach((array) => {
-        array[-1] = Infinity;
-        array[n] = Infinity;
-    });
+  matrix.forEach((array) => {
+    array[-1] = Infinity;
+    array[n] = Infinity;
+  });
 
-    for (let row = 1; row < n; row++) {
-        for (let col = 0; col < n; col++) {
-            matrix[row][col] =
-                matrix[row][col] +
-                Math.min(
-                    matrix[row - 1][col - 1],
-                    matrix[row - 1][col],
-                    matrix[row - 1][col + 1]
-                );
-        }
+  for (let row = 1; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      matrix[row][col] += Math.min(
+        matrix[row - 1][col - 1],
+        matrix[row - 1][col],
+        matrix[row - 1][col + 1]
+      );
     }
+  }
 
-    return Math.min(...matrix[n - 1]);
+  return Math.min(...matrix[n - 1]);
 }
 
 // @lc code=end
