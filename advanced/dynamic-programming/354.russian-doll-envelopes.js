1f91b474bfb93dedfdbbae4445e0da5f03bad2b5
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-06-26 15:24:11 +0800

    Redo 300, 354

--- advanced/dynamic-programming/354.russian-doll-envelopes.js
+++ advanced/dynamic-programming/354.russian-doll-envelopes.js
@@ -10,59 +10,46 @@
  * @return {number}
  */
 
-// O(n^2) / O(n)
+// O(nlogn) / O(n)
 function maxEnvelopes(envelopes) {
-    const heights = envelopes
-        .sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : b[1] - a[1]))
-        .map((envelope) => envelope[1]);
+  const heights = envelopes
+    .sort((a, b) => (a[0] === b[0] ? b[1] - a[1] : a[0] - b[0]))
+    .map((envelope) => envelope[1]);
 
-    return lengthOfLIS(heights);
-}
-
-// O(n^2) / O(n)
-function lengthOfLIS(nums) {
-    const n = nums.length;
-    const dp = new Array(n).fill(1);
-
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            if (nums[i] > nums[j]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-            }
-        }
-    }
-
-    return Math.max(...dp);
+  return lengthOfLIS(heights);
 }
 
 // O(nlogn) / O(n)
 function lengthOfLIS(nums) {
-    const top = [];
-    let piles = 0;
+  const res = [];
 
-    top[0] = nums[0];
-    piles = 1;
-
-    for (const num of nums) {
-        let left = 0;
-        let right = piles - 1;
-
-        while (left <= right) {
-            const mid = left + ((right - left) >> 1);
-            if (top[mid] === num) {
-                right = mid - 1;
-            } else if (top[mid] < num) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        if (left === piles) piles++;
-        top[left] = num;
+  for (const num of nums) {
+    if (res.length === 0 || num > res[res.length - 1]) {
+      res.push(num);
+    } else {
+      const index = equalOrGreaterThan(num, res);
+      res[index] = num;
     }
+  }
 
-    return piles;
+  return res.length;
+}
+
+// O(nlogn) / O(1)
+function equalOrGreaterThan(target, nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const mid = left + ((right - left) >> 1);
+    if (nums[mid] >= target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
 }
 
 // @lc code=end
