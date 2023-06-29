2a637ed8f101c0811843c884e94e839d3fdb06c6
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-06-29 14:43:55 +0800

    Finish 169

---
+++ array/169.majority-element.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=169 lang=javascript
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// O(n) / O(1)
+function majorityElement(nums) {
+  let candidate = null;
+  let count = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+
+    count += num === candidate ? 1 : -1;
+  }
+
+  return candidate;
+}
+
+// @lc code=end
