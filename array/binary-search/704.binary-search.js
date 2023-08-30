cea804adf0ed49945b2882be0286fca9dcd08c13
Luke Mao <ErenJagerM@outlook.com>
2023-08-30 10:03:44 +1000

    Finish 704

--- array/binary-search/704.binary-search.js
+++ array/binary-search/704.binary-search.js
@@ -13,21 +13,21 @@
 
 // O(logn) / O(1)
 function search(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left <= right) {
-        const mid = left + ((right - left) >> 1);
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+  while (left <= right) {
+    const mid = left + ((right - left) >> 1);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
 // @lc code=end
